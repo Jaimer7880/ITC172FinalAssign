3b212ad68f5eddb1e61bf1ac9abbd0bf02641aa3
Jaimer7880 <jaime7880@hotmail.com>
2014-06-17 11:28:58 -0700

    Final Project for ITC172

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcApplicationFinal.MvcApplication" Language="C#" %>
---
+++ Models/DonorInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplicationFinal.Models
+{
+    public class Person
+    {
+        //[Key]public int ID { get; set; }
+        [Key]
+        public int PersonKey { get; set; }
+        public string PersonFirstName { get; set; }
+        public string PersonLastName { get; set; }
+        public string PersonUsername { get; set; }
+     
+
+    }
+    public class DonorDBContext : DbContext
+    {
+        public DbSet<Person> Donors{ get; set; }
+    }
+}
